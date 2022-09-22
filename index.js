0db5f27d88ac55db034f46d50e8c879f2197984b
syblackwell <syblackwell@anywhichway.com>
2022-09-22 15:03:34 -0700

    Wrapped typeSetShadow in a try/catch block.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const self = window.currentComponent(import.meta.url);
 
 self.properties({
     render() {
-        debugger;
+        //debugger;
         const open = this.style.display==="block" ? "[" : "(",
             close = this.style.display==="block" ? "]" : ")";
         this.shadowRoot.innerHTML =
             '<mjx-doc><mjx-head></mjx-head><mjx-body>\\' + open + this.innerHTML + '\\' + close + '</mjx-body></mjx-doc>';
-        MathJax.typesetShadow(this.shadowRoot);
+        try {
+            MathJax.typesetShadow(this.shadowRoot);
+        } catch(e) {
+
+        }
         this.setAttribute("title",this.textContent);
     }
 });
